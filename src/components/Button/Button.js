1bd7623250f137283e1460a924f621a6aed16416
Thomas van Beek <thomas@a-boss.net>
2019-05-10 18:08:18 +0200

    First tests

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,64 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import styled from 'styled-components';
 
-export const Button = ({ text }) => {
-  return <button>{text}</button>;
+export const Button = ({ buttonValue, backgroundColor, backgroundColorLightness }) => {
+  return (
+    <div>
+      <ABOSSButton
+        backgroundColor={backgroundColor}
+        backgroundColorLightness={backgroundColorLightness}
+      >
+        {buttonValue}
+      </ABOSSButton>
+    </div>
+  );
 };
 
 Button.propTypes = {
-  text: propTypes.string
+  buttonValue: propTypes.string
 };
 
 Button.defaultProps = {
-  text: 'Default Prop Text'
+  buttonValue: 'Default Prop Text'
 };
+
+const returnBackgroundColor = (color, lightness = 500) => {
+  switch (color) {
+    case 'primary':
+      switch (lightness) {
+        case '300':
+          return '#E5EDFF';
+        default:
+          return '#004CFF';
+      }
+    case 'warning':
+      return '#FF0236';
+    case 'warning':
+      return '#FF0236';
+    case 'positive':
+      return '#00CC4B';
+    case 'alert':
+      return '#FF9900';
+    case 'primary-gray':
+      return '#5571AA';
+    case 'gray':
+      return '#808080';
+    default:
+      return '#000000';
+  }
+};
+
+const ABOSSButton = styled.button`
+  border-radius: 5px;
+  padding: 15px 25px;
+  background: ${props =>
+    returnBackgroundColor(props.backgroundColor, props.backgroundColorLightness)};
+  border: 0;
+  font-family: 'SF UI Display';
+  font-size: 15px;
+  font-weight: 600;
+  color: ${props =>
+    props.backgroundColorLightness && props.backgroundColorLightness < 400 ? '#000719' : '#fff'};
+  -webkit-font-smoothing: antialiased;
+`;
--- src/components/Button/stories/Basic.js
+++ src/components/Button/stories/Basic.js
@@ -2,7 +2,41 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from 'vaals';
 const BasicButtons = props => {
-  return <Button label="Default" onClick={() => {}} {...props} />;
+  return (
+    <div>
+      <Button />
+      <Button label="Default" buttonValue="Continue" onClick={() => {}} {...props} />
+      <Button
+        label="Default"
+        buttonValue="Continue"
+        backgroundColor="primary"
+        onClick={() => {}}
+        {...props}
+      />
+      <Button
+        label="Default"
+        buttonValue="Continue"
+        backgroundColor="primary"
+        backgroundColorLightness="300"
+        onClick={() => {}}
+        {...props}
+      />
+      <Button
+        label="Default"
+        buttonValue="Continue"
+        backgroundColor="warning"
+        onClick={() => {}}
+        {...props}
+      />
+      <Button
+        label="Default"
+        buttonValue="Continue"
+        backgroundColor="alert"
+        onClick={() => {}}
+        {...props}
+      />
+    </div>
+  );
 };
 
 storiesOf('Button', module).add('Basic', () => <BasicButtons />);
