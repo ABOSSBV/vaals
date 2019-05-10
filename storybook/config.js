564bb6e267f50b4edcfb056081749ba87f5eeafe
Michael Abdulai <michael@a-boss.net>
2019-05-11 00:15:51 +0200

    GlobalStyling and Vaals logo in Storybook

--- storybook/config.js
+++ storybook/config.js
@@ -2,15 +2,24 @@ import { addDecorator, configure } from '@storybook/react';
 import { withOptions } from '@storybook/addon-options';
 import centered from '@storybook/addon-centered/react';
 import ABOSSTheme from './theme.js';
-
+import React, { Fragment } from 'react';
+import { GlobalStyles } from './global-style';
 const req = require.context('../src', true, /\.stories\.js$|\/stories\/.*\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
+function withGlobalStyles(storyFn) {
+  return (
+    <React.Fragment>
+      <GlobalStyles />
+      {storyFn()}
+    </React.Fragment>
+  );
+}
+addDecorator(withGlobalStyles);
 addDecorator(centered);
-
 addDecorator(
   withOptions({
     theme: ABOSSTheme
